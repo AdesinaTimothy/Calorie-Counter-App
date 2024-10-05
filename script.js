c034ec155d98d9531f45b9bed47273153137d30a
Timothy Adesina <timobest121@gmail.com>
2024-10-05 11:50:25 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const walkingMinutes = document.getElementById('walking');
+const runningMinutes = document.getElementById('running');
+const cyclingMinutes = document.getElementById('cycling');
+const calculateBtn = document.getElementById('cal')
+const totalCalories = document.getElementById('total-calories')
+
+function calculateCalories () {
+    const caloriesByWalking = walkingMinutes.value * 5; 
+    const caloriesByRunning = runningMinutes.value * 10;
+    const caloriesByCycling = runningMinutes.value * 8;
+
+    total = caloriesByWalking + caloriesByRunning + caloriesByCycling;
+    totalCalories.innerText = `Total Calories Burnt: ${total}`;
+};
+
+calculateBtn.addEventListener('click', calculateCalories);
+
+
